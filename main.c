68cf4749ed843f5963fdb9e4fbff95720cbca7f9
Patrick Brem <pbrem@aei.mpg.de>
2012-07-06 11:05:46 +0200

    Added new input format: 2 BHs now precede the gas particle data in the same input file Now 2 seperate outputs: out.dat for TD at BH1, out2.dat for TD at BH2.

--- main.c
+++ main.c
@@ -9,27 +9,34 @@ Patrick Brem, AEI Potsdam, 07/2012 */
 #define VSCALE 613476.295
 #define TIMEINDAYS 23286.24 
 
-void loopthroughgas(double* bhpos, double clight, double phi, double theta)
+void loopthroughgas(double clight, double phi, double theta)
 {
   int id;
   double mass,rij,baselambda,lambda,time,rij_eye;
-  FILE *f, *fout;
+  FILE *f, *fout, *fout2;
   char line[160];
-  double tpos[3],tv[3];
+  double tpos[3],tv[3],bh1pos[3],bh1v[3],bh2pos[3],bh2v[3];
   double rotate[3],v,strength;
   baselambda = 656.0;
   f = fopen("gas.dat", "rt");
   fout = fopen("out.dat", "w");
+  fout2 = fopen("out2.dat", "w");
   //define rotation matrix; need only to obtain the final z component so only need last line of the matrix, e.g. a vector;
   theta = theta/180.0*3.14159;
   phi = phi/180.0*3.14159;
   rotate[0] = sin(theta)*sin(phi);
   rotate[1] = sin(theta)*cos(phi);
   rotate[2] = cos(theta);
+  //read the 2 BHs
+  fgets(line,160,f);
+  sscanf(line, "%d %lg %lg %lg %lg %lg %lg %lg\n",&id,&mass,&bh1pos[0],&bh1pos[1],&bh1pos[2],&bh1v[0],&bh1v[1],&bh1v[2]);
+  fgets(line,160,f);
+  sscanf(line, "%d %lg %lg %lg %lg %lg %lg %lg\n",&id,&mass,&bh2pos[0],&bh2pos[1],&bh2pos[2],&bh2v[0],&bh2v[1],&bh2v[2]);
   while (fgets(line, 160, f) != NULL)
     {
       sscanf(line, "%d %lg %lg %lg %lg %lg %lg %lg\n",&id,&mass,&tpos[0],&tpos[1],&tpos[2],&tv[0],&tv[1],&tv[2]);
-      rij = sqrt((tpos[0]-bhpos[0])*(tpos[0]-bhpos[0])+(tpos[1]-bhpos[1])*(tpos[1]-bhpos[1])+(tpos[2]-bhpos[2])*(tpos[2]-bhpos[2]));
+      rij = sqrt((tpos[0]-bh1pos[0])*(tpos[0]-bh1pos[0])+(tpos[1]-bh1pos[1])*(tpos[1]-bh1pos[1])+(tpos[2]-bh1pos[2])*(tpos[2]-bh1pos[2]));
+      rij2 = sqrt((tpos[0]-bh2pos[0])*(tpos[0]-bh2pos[0])+(tpos[1]-bh2pos[1])*(tpos[1]-bh2pos[1])+(tpos[2]-bh2pos[2])*(tpos[2]-bh2pos[2]));
       //need also distance to the observer, who is located at the direction (phi,theta) in degrees
       //rotation so that z points into observer direction (confirm definitions/notations.....)
       rij_eye = rotate[0]*tpos[0]+rotate[1]*tpos[1]+rotate[2]*tpos[2];
@@ -38,8 +45,11 @@ void loopthroughgas(double* bhpos, double clight, double phi, double theta)
       time = (rij+rij_eye)/clight*TIMEINDAYS; //when the TD reaches the gas particle and then the radiation reaches the eye
       lambda = baselambda/(1.0-v/clight);
       strength = 1.0/(rij*rij);
+      time2 = (rij2+rij_eye)/clight*TIMEINDAYS; //when the TD reaches the gas particle and then the radiation reaches the eye
+      strength2 = 1.0/(rij2*rij2);
       //strength is because of the weaker TD impact over distance^2
       fprintf(fout,"%f\t%f\t%f\n",time,lambda,strength);
+      fprintf(fout2,"%f\t%f\t%f\n",time2,lambda,strength2);
     }
 
 }
@@ -48,10 +58,7 @@ void loopthroughgas(double* bhpos, double clight, double phi, double theta)
 int main (int argc, char** argv)
 {
 
-  double bhpos[3], clight, phi, theta;
-  bhpos[0] = 0.012866;
-  bhpos[1] = 0.3103;
-  bhpos[2] = -0.02399;
+  double clight, phi, theta;
   clight = 3.0e8/VSCALE;
   phi = 0.0;
   theta = 45.0;
